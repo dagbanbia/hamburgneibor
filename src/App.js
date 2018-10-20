6bd0a072444c0a5e2f6018d4a0c0071e279c0c0d
Dagbanbia Mohammed Zaid <dagbanbia@yahoo.com>
2018-10-20 07:32:33 +0200

    update of the menu part

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-
 import './App.css';
-
-import axios from 'axios'
-
+import axios from 'axios';
+import escapeRegExp from 'escape-string-regexp';
 import Header from './Header';
+import SearchBar from './SearchBar';
+import MenuComponent from './MenuComponent';
+import ErrorBoundary from './ErrorBoundary';
+
 
 class App extends Component {
 
 
-   state = {
-    venues : []
-   }
+constructor(props) {
+    super(props)
+    this.state = {
+      venues: [],
+      markers: [],
+      showVenues: [],
+      query: '',
+      notVisibleMarkers: []
+  }}
 
     componentDidMount(){
       this.getVenues()
@@ -38,7 +46,8 @@ class App extends Component {
      axios.get(endPoint + new URLSearchParams(parameters))
       .then(response =>{
         this.setState({
-          venues:response.data.response.groups[0].items
+          venues:response.data.response.groups[0].items,
+          showVenues: response.data.response.groups[0].items
         }, this.renderMap())
     })
       
@@ -61,44 +70,114 @@ class App extends Component {
      //Display Dynamic Markers
     this.state.venues.map(myVenue => {
 
-      var contentString = `${myVenue.venue.name}`
+      var contentString = `<b>${myVenue.venue.name}</b> <br><i>${myVenue.venue.location.address}</i>`
 
      //Create A Marker
 
     var marker = new window.google.maps.Marker({
       position: {lat: myVenue.venue.location.lat,lng:myVenue.venue.location.lng},
       map: map,
+      animation: window.google.maps.Animation.DROP,
       title:myVenue.venue.name
     
   });
 
-    //Click on A Marker
-    marker.addListener('click', function(){
+  this.state.markers.push(marker)
 
-      //Change the content
-      infowindow.setContent(contentString)
+  function animationEffect() {
+        marker.setAnimation(window.google.maps.Animation.BOUNCE)
+        setTimeout(function(){ marker.setAnimation(null) }, 550)
+      }
 
-      //open An Infowindow
-      infowindow.open(map,marker)
+      function openMarker() {
+        // Setting the content of the InfoWindow
+        infowindow.setContent(contentString)
+        animationEffect()
+        
+      // Open an InfoWindow upon clicking on its marker
+        infowindow.open(map, marker)
+      }
+
+
+      // Click on a marker
+      marker.addListener('click', function() {
+        openMarker()
+
     })
 
-    })
+    }
+  )
 
   }
+
+   
+  updateQuery = query => {
+    this.setState({ query })
+    this.state.markers.map(marker => marker.setVisible(true))
+    let filterVenues
+    let notVisibleMarkers
+
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), "i")
+      filterVenues = this.state.venues.filter(myVenue =>
+        match.test(myVenue.venue.name)
+      )
+      this.setState({ venues: filterVenues })
+      notVisibleMarkers = this.state.markers.filter(marker =>
+        filterVenues.every(myVenue => myVenue.venue.name !== marker.title)
+      )
+
+      /* 
+       * Hiding the markers for venues not included in the filtered venues
+      */
+      notVisibleMarkers.forEach(marker => marker.setVisible(false))
+
+      this.setState({ notVisibleMarkers })
+    } else {
+      this.setState({ venues: this.state.showVenues })
+      this.state.markers.forEach(marker => marker.setVisible(true))
+    }
+  }
+
   render() {
-    return (
-
+    if (this.state.hasError) {
+      return <div id="Error-message" aria-label="Error message">Sorry, something went wrong!</div>
+    } else {
+      return (
       <main>
-
-      <div id="header" aria-label="Header">
-          <Header />
-      </div>
-       <div id="map"></div>
-
-      </main>
-      
+        <ErrorBoundary>
+        
          
+        <div id="header" aria-label="Header">
+          <Header />
+        </div>
+
+        <div id="SearchBar" aria-label="Search Bar">
+          <SearchBar
+            venues={ this.state.showVenues } 
+            markers={ this.state.markers } 
+            filteredVenues={ this.filteredVenues }
+            query={this.state.query}
+            clearQuery={this.clearQuery}          
+            updateQuery={b => this.updateQuery(b)}
+            clickLocation={this.clickLocation}
+          />
+        </div>
+        
+        <div id="container" aria-label="Menu Container">
+          <MenuComponent 
+            venues={ this.state.venues }
+            markers={ this.state.markers }
+          />
+        </div>
+
+        <div id="map" aria-label="Map" role="application">
+        </div>
+
+        </ErrorBoundary>
+      </main>
     );
+  }
   }
 }
 
